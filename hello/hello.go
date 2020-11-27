c0c5c25d9727d729d7696f57377eee29a36b2439
georgep9 <g-patt@outlook.com>
2020-11-28 00:45:35 +1000

    error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
 
-	message := greetings.Hello("Gladys")
+	// set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greetings message.
+	message, err := greetings.Hello("")
+
+	// if an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if no error was returned, print the returnd message
+	// to the console.
 	fmt.Println(message)
 }
